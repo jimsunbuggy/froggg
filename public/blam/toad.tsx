4a7bbb5230ee1ef4cc10c864cdc2cf523729f815
jimsunbuggy <108547137+jimsunbuggy@users.noreply.github.com>
2024-10-13 14:58:57 -0700

    Update toad.tsx in blam under public

--- public/blam/toad.tsx
+++ public/blam/toad.tsx
@@ -1,1 +1,15 @@
+import '@/app/ui/global.css';
+import { inter, dancing_script } from '@/app/ui/fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${dancing_script.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
 
