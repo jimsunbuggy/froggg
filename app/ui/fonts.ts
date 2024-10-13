e1e8ac0da90e9d917376af63530ff671d91abee3
jimmysunbuggy <jimmy@sunbuggy.com>
2024-10-13 13:31:03 -0700

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Dancing_Script } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+export const dancing_script = Dancing_Script({
+    subsets: ['latin'],
+    display: 'swap',
+  })
